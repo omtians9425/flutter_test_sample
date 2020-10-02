3924f132b2145339ef080bb90b7998d20901c4eb
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-10-03 02:08:24 +0900

    setup performance test

---
+++ test_driver/app_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Testing App Performance Tests', (){
+    FlutterDriver driver;
+
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    tearDownAll(() async{
+      if (driver != null) {
+        await driver.close();
+      }
+    });
+  });
+}
